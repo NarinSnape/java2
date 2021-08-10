8b535fb0c2ee395e5622a436b1a3ca25b10da5c8
Anastasia Guschina <agushhina@appline.ru>
2021-08-11 00:56:23 +0300

    7 задание: обработка исключений

--- src/main/java/calculator/Program.java
+++ src/main/java/calculator/Program.java
@@ -1,15 +1,24 @@
 package calculator;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Введите поочередно два числа и знак операции (‘+’, ‘-’, ‘*’ или ‘/’): ");
+        System.out.println("Введите поочередно два целых числа и знак операции (‘+’, ‘-’, ‘*’ или ‘/’): ");
         Scanner scanner = new Scanner(System.in);
-        double x = scanner.nextDouble();
-        double y = scanner.nextDouble();
-        String calc = scanner.next();
+        int x = 0, y = 0;
+        String calc = "";
+        try {
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+            calc = scanner.next();
+        } catch (InputMismatchException ime) {
+            System.out.println("Неверные данные, попробуйте снова.");
+        }
+
         scanner.close();
         Calculator calculator = new Calculator(x, y, calc);
-        System.out.println(calculator.Processing());
+        calculator.Processing();
     }
 }
